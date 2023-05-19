54c7d0474eda4425b973b05f3290e35bb52d3b5b
Drajner <drajner@wp.pl>
2023-05-19 23:53:22 +0200

    added getters

--- GameStatus.hs
+++ GameStatus.hs
@@ -2,10 +2,15 @@ module GameStatus where
 
 import CurrentPosition
 import Flag
-
+import Control.Monad.State
 
 data GameStatus = GameStatus
     { currentPosition :: CurrentPosition
     , flags :: [Flag]
-
     }
+
+getFlags :: State GameStatus [Flag]
+getFlags = gets flags
+
+getCurrentPosition :: State GameStatus CurrentPosition
+getCurrentPosition = gets currentPosition
