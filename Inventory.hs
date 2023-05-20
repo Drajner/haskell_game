94d7fec5809c5b75211245347e726e694b1101ef
Drajner <drajner@wp.pl>
2023-05-20 02:51:43 +0200

    created some shit for inventory and changed some details in map

---
+++ Inventory.hs
@@ -0,0 +1,11 @@
+
+import Control.Monad.State
+
+type Inventory = [String]
+
+addToInventory :: String -> State Inventory ()
+addToInventory newItem = modify(\s -> s ++ [newItem])
+
+printInventory :: Inventory -> IO ()
+printInventory = mapM_ putStrLn
+
--- map.hs
+++ map.hs
@@ -30,8 +30,8 @@ givePositionModificationText 2 pos = "Kapitan Bomba wlasnie tam jest"
 givePositionModificationText 3 pos = "Niestety nie da sie tam pojsc."
 
 
-mapLoop :: CurrentPosition -> Flag -> IO ()
-mapLoop pos = do
+mapLoop :: (CurrentPosition, Flag) -> IO ()
+mapLoop (pos, airlockFlag) = do
 
     let mes = evalState getPosition pos
     putStrLn ("START" ++ mes)
@@ -44,20 +44,20 @@ mapLoop pos = do
                 let ((), finalState) = runState (changePosition input) pos
                 let mes2 = evalState getPosition finalState
                 putStrLn ("END" ++ mes2)
-                mapLoop finalState airlockFlag
+                mapLoop (finalState, airlockFlag)
         2 -> do
                 putStrLn ("END" ++ mes)
-                mapLoop pos airlockFlag
+                mapLoop (pos, airlockFlag)
         3 -> do
                 putStrLn ("END" ++ mes)
-                mapLoop pos airlockFlag
+                mapLoop (pos, airlockFlag)
 
 
 main :: IO ()
 main = do 
     let pos = CurrentPosition {position = "pokoj"}
     let airlockFlag = startFlag "sluzaOtwarta"
-    mapLoop pos airlockFlag
+    mapLoop (pos, airlockFlag)
 
 
 
